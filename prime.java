1e728db57e39c00497a8f43fbc407e27084f575c
Naman9493 <namanchauhan150@gmail.com>
2025-08-17 21:54:27 +0530

    Prime in range

--- prime.java
+++ prime.java
@@ -1,3 +1,25 @@
+import java.util.*;
 public class prime {
-    
+     public static boolean isprime(int a) {
+           for(int i=2;i<=a-1;i++){
+            if(a%i==0){
+              return false;}
+           }
+          return true;
+        }
+        public static void primein_range(int n) {
+            for(int i=2;i<n;i++){
+                if(isprime( i)){
+                    System.out.print(i+" ");
+                }
+            }
+            System.out.println();
+        }
+        public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            System.out.print("Enter range: ");
+            int a=sc.nextInt();
+            primein_range(a);
+            
+        }
 }
